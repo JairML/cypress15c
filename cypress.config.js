1d306725c6c3d7bb65ecaa9481418d06fa55df67
JairML <jairmenendez09@gmail.com>
2026-06-28 19:19:01 -0500

    Optimizar memoria de Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,9 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000/',
+    baseUrl: 'https://labs15c-kum941qfh-ados-projects-0eb7f124.vercel.app/',
+    numTestsKeptInMemory: 0,
+    experimentalMemoryManagement: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
